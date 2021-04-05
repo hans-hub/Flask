b0e9a9b3f14c6484aba9d92b70c6618137160060
Baffour Hans <baffourhans@gmail.com>
2021-04-05 22:58:53 +0800

    New

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,30 +1,20 @@
+import secrets,os
+from PIL import Image
 from flaskblog.models import User,Post
 from flaskblog import app,db,bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm 
+from flaskblog.forms import RegistrationForm, LoginForm,UpdateAccountForm,PostForm
 from flask import render_template, url_for, flash, redirect,request
 from flask_login import login_user,current_user,logout_user,login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date _posted': 'April 21, 2018'
-    }
-]
 
 
+#Home route
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    posts = Post.query.all()
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
@@ -43,7 +33,7 @@ def register():
         db.session.commit()
         flash(f'Your account has been created! You are now able to login', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form) 
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -68,9 +58,57 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
+#save picture function
+def save_picture(form_picture):
+     random_hex = secrets.token_hex(8)
+     _,f_ext = os.path.splitext(form_picture.filename)
+     picture_fn = random_hex + f_ext
+     picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+     
+     #resizing the profile picture
+     output_size = (125,125)
+     i = Image.open(form_picture)
+     i.thumbnail(output_size)
+     
+     i.save(picture_path)
+     return picture_fn
+
+
+
 #creating account route
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been update!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'POST':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        
     image_file = url_for('static',filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html',title='Account',image_file=image_file)
+    return render_template('account.html',title='Account',
+                           image_file=image_file,form=form)
+    
+    #creating Post route
+    
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data,content = form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
